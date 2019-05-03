f2ae7decf6535c9a5c01fe096d22679d9339aef9
Joe-noh <goflb.jh@gmail.com>
2019-05-04 00:40:06 +0900

    style on outline and disabled

--- packages/button/src/Button.svelte
+++ packages/button/src/Button.svelte
@@ -1,12 +1,43 @@
 <script>
+  export let outline = false
+  export let disabled = false
+
+  $: normal = !outline
 </script>
 
 <style>
-  .button {
-    color: goldenrod;
+  button {
+    cursor: pointer;
+    border-radius: 4px;
+    font-size: 1rem;
+    margin: 0;
+    padding: 8px 16px;
+  }
+  button:disabled {
+    cursor: not-allowed;
+  }
+
+  .normal {
+    color: white;
+    background-color: #0066FF;
+    border: none;
+  }
+  .normal:disabled {
+    background-color: #ccc;
+  }
+
+  .outline {
+    color: #0066FF;
+    background-color: white;
+    border: 1px solid #0066FF;
+  }
+  .outline:disabled {
+    color: #ccc;
+    background-color: white;
+    border: 1px solid #ccc;
   }
 </style>
 
-<button class="button">
+<button class:normal class:outline { disabled }>
   <slot />
 </button>
